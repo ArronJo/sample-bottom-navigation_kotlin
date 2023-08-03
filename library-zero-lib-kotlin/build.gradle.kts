8889c0a5a8c4519f98fa97acd23e68fd8dfd1c33
mcharima5 <mcharima5@gmail.com>
2023-08-03 17:25:12 +0900

    feat: Migrating Groovy DSL to Kotlin DSL

---
+++ library-zero-lib-kotlin/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.snc.zero.lib.kotlin"
+    compileSdk = rootProject.extra["target_sdk_version"] as Int
+    compileSdkPreview = "UpsideDownCake"
+
+    defaultConfig {
+        minSdk = rootProject.extra["min_sdk_version"] as Int
+        //targetSdk = rootProject.extra["target_sdk_version"] as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra["java_compiler_version"] as JavaVersion
+        targetCompatibility = rootProject.extra["java_compiler_version"] as JavaVersion
+    }
+    kotlin {
+        jvmToolchain(rootProject.extra["jvm_toolchain"] as Int)
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra["java_compiler_version"].toString()
+    }
+}
+
+dependencies {
+    //implementation(project(path: ":library-zero-androidx-kotlin"))
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    implementation("io.github.ParkSangGwon:tedpermission:3.3.0")
+    implementation("io.github.ParkSangGwon:tedpermission-normal:3.3.0")
+}
