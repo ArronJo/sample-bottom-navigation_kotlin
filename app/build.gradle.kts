ab7e8d37b7f97d6a3d15f4d6d4b74d1ea3050b56
mcharima5 <mcharima5@gmail.com>
2023-10-13 09:13:10 +0900

    chrome: 라이브러리 최신화

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    //id("com.google.devtools.ksp")
 }
 
 android {
@@ -149,7 +150,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -165,8 +166,8 @@ dependencies {
 
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
     implementation("androidx.webkit:webkit:1.8.0")
 
