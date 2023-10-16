2ee0da186192ddefa0d76af4d745b319be576ea0
mcharima5 <mcharima5@gmail.com>
2023-10-16 13:28:53 +0900

    feat: gradle groovy -> kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Define versions in a single place
+buildscript {
+    extra.apply {
+        set("java_compiler_version", JavaVersion.VERSION_17)
+        set("jvm_toolchain", 17)
+        set("min_sdk_version", 23)
+        set("target_sdk_version", 34)
+    }
+    repositories {
+        // Make sure that you have the following two repositories
+        google()  // Google's Maven repository
+
+        mavenCentral()  // Maven Central repository
+    }
+    dependencies {
+
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id ("com.android.application") version "8.1.0" apply false
+    id ("com.android.library") version "8.1.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("io.gitlab.arturbosch.detekt").version("1.19.0")
+}
