952e51831db12942b25a90137dd35864073d5388
mcharima5 <mcharima5@gmail.com>
2024-01-12 12:54:22 +0900

    feat: AGP 8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -166,7 +166,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -183,8 +183,8 @@ dependencies {
 
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
     implementation("androidx.webkit:webkit:1.9.0")
 
